eb90825b871155bda7f7b0d82c50961975de6f19
Amin <aminakbari733@gmail.com>
2024-03-11 18:16:48 +0330

    Update showpost.blade.php

--- resources/views/showpost.blade.php
+++ resources/views/showpost.blade.php
@@ -221,43 +221,8 @@
                 <div class="middle-column">
                     <div class="card">
                         <div class="card-body">
-                            <div class="media">
-                                <img src="{{asset('storage/images/'.$post->image)}}" alt="img" width="55px" height="55px"
-                                    class="rounded-circle mr-3">
-                                <div class="media-body">
-                                    <h5>Mark Otto</h5>
-                                    <p class="text-justify">{{ $post->body }}</p>
-                                    <a href="{{asset('storage/images/'.$post->image)}}" data-lightbox="id"><img
-                                            src="{{asset('storage/images/'.$post->image)}}" alt=""
-                                            class="img-fluid shadow-sm img-thumbnail"> </a>
-                                </div>
-                                <small class="text-muted">10 min</small>
-                            </div>
-                        <hr>
-                        <div class="media mb-3" style="margin-left: -10px">
-                            <img src="{{ asset('assets/img/avatar-dhg.png')}}" alt="img" width="45px" height="45px" class="rounded-circle mr-2">
-                            <div class="media-body">
-                                    <p class="card-text text-justify">Jacon Thornton: Donec id elit non mi porta gravida at eget metus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis.</p>
 
-                            </div>
-                        </div>
-                        <hr>
-                        <form class="form-inline" wire:submit="addpost">
-                            <div class="input-group w-100">
-                                <textarea wire:model="body" type="text" name="message" id="message" placeholder="Message"class="form-control form-control-md">
-                                </textarea>
-                                <br>
-                            </div>
-
-                              <button class="btn btn-success" type="submit" style="margin-left: 150px;margin-top:10px ;width:40%">send</button>
-                        </form>
-
-
-
-
-
-
-
+                 @livewire('commentlivewire',['post' => $post])
                 </div>
                     </div>
                 </div>
