ce51db86d679b1aca4ee9f277440f9f6956dd41b
Amin <aminakbari733@gmail.com>
2024-03-11 19:39:01 +0330

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password')->default(0);
-
+            $table->string('info')->default(0);
             $table->string('birthday')->default(0);
             $table->string('avatar')->default(0);
             $table->string('active')->default(1);
