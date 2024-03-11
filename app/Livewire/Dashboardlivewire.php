516445937a4dd65b95f4a0de2a2dcc99e89c8022
Amin <aminakbari733@gmail.com>
2024-03-11 23:13:39 +0330

    Update Dashboardlivewire.php

--- app/Livewire/Dashboardlivewire.php
+++ app/Livewire/Dashboardlivewire.php
@@ -12,20 +12,33 @@ use Storage;
 class Dashboardlivewire extends Component
 {
     use WithFileUploads;
-    public $image ;
-    public $body;
+
+    public $image="" ;
+    public $body="";
     public $posts;
     public function addpost()
     {
-
-        $namei = md5($this->image . microtime()).'.'.$this->image->extension();
-        $this->image->storeAs('images', $namei,'public');
+        if($this->image=="" & $this->body!="")
+        {
         post::Create([
-        'body'=>$this->body,
-        'image'=>$namei,
-        'user_id'=>auth()->user()->id,
-        ]);
-        Session()->flash('msg', 'اضافه شد!');
+            'body'=>$this->body,
+            'image'=>'0',
+            'user_id'=>auth()->user()->id,
+            ]);
+            Session()->flash('msg', 'اضافه شد!');
+        }
+        if($this->image!="" & $this->body!="")
+        {
+            $namei = md5($this->image . microtime()).'.'.$this->image->extension();
+            $this->image->storeAs('images', $namei,'public');
+            post::Create([
+            'body'=>$this->body,
+            'image'=>$namei,
+            'user_id'=>auth()->user()->id,
+            ]);
+            Session()->flash('msg', 'اضافه شد!');
+        }
+
         $this->body="";
         $this->image="";
         $this->render();
@@ -34,6 +47,7 @@ class Dashboardlivewire extends Component
 
     public function render()
     {
+
         $this->posts=post::select()->where('user_id', auth()->user()->id)->where('active','1')->orderByDesc('created_at')->get();
         return view('livewire.dashboardlivewire');
     }
