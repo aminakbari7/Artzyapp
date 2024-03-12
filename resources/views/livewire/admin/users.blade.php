eb8054114da3331b319db36742124739187021cc
Amin <aminakbari733@gmail.com>
2024-03-12 17:25:40 +0330

    Create users.blade.php

---
+++ resources/views/livewire/admin/users.blade.php
@@ -0,0 +1,31 @@
+<div class="bg-light rounded h-100 p-4">
+    <h6 class="mb-4">user list</h6>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">birthday</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user )
+                <tr>
+                    <th scope="row">3</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->address }}</td>
+                    <td>{{ $user->birthday }}</td>
+                    <td>{{ $user->active }}</td>
+                    <td> action</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
