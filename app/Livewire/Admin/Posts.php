4b4fec4deea5cab2b8869d78759be62a52745bfd
Amin <aminakbari733@gmail.com>
2024-03-14 10:15:47 +0330

    Update Posts.php

--- app/Livewire/Admin/Posts.php
+++ app/Livewire/Admin/Posts.php
@@ -3,16 +3,15 @@
 namespace App\Livewire\Admin;
 use App\Models\post;
 use Livewire\Component;
-
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 class Posts extends Component
 {
-    public $posts;
-    public function mount()
-    {
-        $this->posts = Post::all();
-    }
+    use WithPagination;
     public function render()
     {
-        return view('livewire.admin.posts');
+        $posts = post::select()->orderByDesc('created_at')->paginate(5);
+
+        return view('livewire.admin.posts',['posts'=> $posts]);
     }
 }
